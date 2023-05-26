ec4e94a67bdf06d9f366314fded824d19ccad52b
benyqhao <86880120+benyqhao@users.noreply.github.com>
2023-05-26 18:47:16 +0800

    Update convertJSON.js
    Change mouseover to mouse click for popups.

--- js/ext/convertJSON.js
+++ js/ext/convertJSON.js
@@ -38,15 +38,12 @@ function convertJSON(resp, variable_name, lowColor, highColor) {
       fillOpacity: 0.85,
     },
     onEachFeature: function (feature, layer) {
-      layer.on('mouseover', function (e) {
+      layer.on('click', function (e) {
         layer.bindPopup(
           '<h2>' + feature.properties.suburb_name + '</h2><b>' + 
           variable_name + ':</b> ' + feature.properties.value + '</p>'
           )
         this.openPopup();
-        });
-      layer.on('mouseout', function (e) {
-        this.closePopup();
         });
     },
   }).addTo(map);
